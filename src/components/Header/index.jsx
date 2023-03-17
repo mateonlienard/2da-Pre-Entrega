8bf1839af29e31d7de0270d406eff7abd55ad905
mateonlienard <mateonlienard@gmail.com>
2023-03-17 16:50:42 -0300

    cambie la ruta del logo de la empresa

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,10 @@
 import './header.css'
+import monitorled from '../../assets/monitorled.png'
 
 function Header(){
     return(
     <div className='header'>
-        <div className="icono"><img src="Imagenes/monitorled.png" alt="Logo empresa" style={{width: '150px', height: '150px'}} /></div>
+        <div className="icono"><img src={monitorled} alt="Logo empresa" style={{width: '150px', height: '150px'}} /></div>
         <h1 className="bienvenida">¡Seguí confiando en nosotros!</h1>
     </div>
     )
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,4 +1,3 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ItemCount from '../ItemCount/index'
 import { Link } from 'react-router-dom';
