de5354924bb5a563799f09333c61718bf066dd32
mateonlienard <mateonlienard@gmail.com>
2023-03-16 21:47:16 -0300

    correccion

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ function NavBar(){
     return (
                     <nav className="lugar-tabla">
                         <ul className="tabla">
-                            <li className='d-flex justify-content-start'><a className=' articulo' href="index.html">Inicio</a></li>
+                            <li className='d-flex justify-content-start'><a className=' articulo' href="">Inicio</a></li>
                             <CartWidget />
                         </ul>
                     </nav>
