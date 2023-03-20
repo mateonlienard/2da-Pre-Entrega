935d8fcb473348d6a47a09b0d9f251b86bdf5767
mateonlienard <mateonlienard@gmail.com>
2023-03-20 19:53:43 -0300

    hice que el footer este siempre al final de la pagina

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,8 +2,8 @@ import './index.css'
 
 function Footer(){
     return(
-        <footer>
-            <div className='footerDiv'>
+        <footer className='footer'>
+            <div>
                 <p className="footerP text-white text-center">Esta app fue creada y desarrollada en su totalidad por Mateo Nuñez</p>
             </div>
         </footer>
