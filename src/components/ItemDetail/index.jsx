f886cc0eebef5a5daf4e14072c154752e4e6fcef
mateonlienard <mateonlienard@gmail.com>
2023-03-20 20:03:12 -0300

    hice arreglos en los estilos de la card en itemDetail, y el footer

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 function ItemDetail({producto}){
     return(
       <div >
-      <Card className='d-flex justify-content-center' style={{ width: '18rem' }}>
+      <Card className='d-flex justify-content-center mb-5' style={{ width: '18rem' }}>
         <Card.Img variant="top" src={producto.img} />
         <Card.Body>
           <Card.Title>{producto.titulo}</Card.Title>
