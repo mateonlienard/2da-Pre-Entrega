242b9d132e143392403e5dcb3685ff62f589418d
mateonlienard <mateonlienard@gmail.com>
2023-03-30 17:58:26 -0300

    llame a mi button desde react-bootsrap, lo estaba llamando desde bootsrapp

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,4 +1,4 @@
-import { Button } from 'bootstrap';
+import { Button } from 'react-bootstrap';
 import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
