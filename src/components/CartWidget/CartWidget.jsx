acdc7b3b78482c38b611b918fc306b10edcb20dd
mateonlienard <mateonlienard@gmail.com>
2023-03-30 18:47:10 -0300

    correccion de errores

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ import './carrito.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
-import { Context } from '../../context';
+import { Context } from '../../context/index';
 import { Link } from 'react-router-dom';
 
 function CartWidget(){
