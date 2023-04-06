37325d3755b46e8bfea0f6e6e771094f2473c4f1
mateonlienard <mateonlienard@gmail.com>
2023-04-06 18:48:22 -0300

    realice toda la logica con firebase en mi carrito

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,20 +1,38 @@
 import { useContext } from "react";
 import { Context } from '../../context/index'
+import {collection, getFirestore, addDoc, doc, updateDoc} from 'firebase/firestore'
 import './index.css'
 
 function Cart(){
     const {cart} = useContext(Context)
+    const db = getFirestore();
 
-    function pagar(){
+    function updateOrder(productoId, finalStock){
+        const itemRef = doc(db, 'Productos', productoId)
+        updateDoc(itemRef, {stock: finalStock})
+        .catch(error=>console.log({error}));
+    }
+
+    function sendOrder(){
         const total = cart.reduce(
             (acc, producto) => acc + producto.cantidad * producto.precio, 0
         )
 
-        {
-            buyer:
-            products: cart
+        const order = {
+            buyer: {name: mateo, mail: 'mateon@gmail.com', phone: '1173618344'},
+            products: cart,
             total
         }
+
+        const collectionRef = collection(db, 'orders');
+        addDoc(collectionRef, order)
+        .then(()=>{
+            cart.map((producto)=>{
+              const finalStock = producto.stock = producto.cantidad
+              updateOrder(producto.id, finalStock)
+            })
+        })
+        .catch(error=>console.log({error}))
     }
 
     return(
@@ -28,7 +46,7 @@ function Cart(){
                     <span>Precio unitario: {producto.precio}</span>
                     <br />
                     <br />
-                    <button onClick={pagar}>Terminar compra</button>
+                    <button onClick={sendOrder}>Terminar compra</button>
                 </div>
             ))}
         </div>
